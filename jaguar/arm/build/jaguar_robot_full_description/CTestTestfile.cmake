4cbf9945639c957370af57941beceae8ff0e80d6
arepo <arepo90@proton.me>
2026-04-02 18:17:05 -0600

    matate y subelo a canvas

--- jaguar/arm/build/jaguar_robot_full_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arepo/TMR2026_Rescue/jaguar/arm/src/jaguar_robot_full_description
-# Build directory: /home/arepo/TMR2026_Rescue/jaguar/arm/build/jaguar_robot_full_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

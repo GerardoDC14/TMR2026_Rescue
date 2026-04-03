c6f420d243d01ea69677e60acbc9ab88e87f9d2d
arepo <arepo90@proton.me>
2026-04-02 18:17:05 -0600

    matate y subelo a canvas

--- jaguar/arm/build/jaguar_robot_full_description/CMakeFiles/jaguar_robot_full_description_uninstall.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for jaguar_robot_full_description_uninstall.
--- jaguar/arm/build/jaguar_teleop/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/arepo/TMR2026_Rescue/jaguar/arm/install/jaguar_teleop'
